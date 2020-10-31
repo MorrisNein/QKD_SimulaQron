c4cb313c0c53469792b901d454065434622eaa91
Peter Shevchenko <petroochcho@gmail.com>
2020-10-31 12:48:59 +0300

    requirements.txt and propper run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-#python3 network_start.py &
-#simulaqron start --keep --nodes 'Alice, Bob, Charlie' --topology '{"Alice": ["Charlie"], "Bob": ["Charlie"], "Charlie": ["Alice", "Bob"]}'
-simulaqron start --keep
 python3 charlieNode.py &
 python3 aliceNode.py
-simulaqron stop
