eb3dbb3f467a3b9baeffd2e6552e8302ac2a55f9
Peter Shevchenko <petroochcho@gmail.com>
2020-10-31 12:24:41 +0300

    now ignoring .DS_Store

--- change_settings.sh
+++
@@ -1,2 +0,0 @@
-simulaqron set max-qubits 100
-simulaqron set network-config-file "/Users/petershevchenko/PycharmProjects/QKD_Simulaqron/venv/lib/python3.8/site-packages/simulaqron/config/network.json"
