be8a160c1a75f07610920efa14786ead9c2e8387
Peter Shevchenko <petroochcho@gmail.com>
2020-10-31 12:26:29 +0300

    fix

---
+++ change_settings.sh
@@ -0,0 +1,2 @@
+simulaqron set max-qubits 100
+simulaqron set network-config-file "/Users/petershevchenko/PycharmProjects/QKD_Simulaqron/venv/lib/python3.8/site-packages/simulaqron/config/network.json"
