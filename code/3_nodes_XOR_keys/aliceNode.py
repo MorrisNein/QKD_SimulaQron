8c2ad68f7d4a13882e2dde20d61722dd00bbaf56
Peter Shevchenko <petroochcho@gmail.com>
2020-11-04 13:52:10 +0300

    directory structure support

---
+++ code/3_nodes_XOR_keys/aliceNode.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('../../')
+
+
+def main():
+    from common.bb84.node import Node
+
+    alice = Node('Alice')
+    alice.transmit_key('Charlie')
+    # Alice then just chills until Bob receives the mixed key from Charlie.
+    # Then Bob knows Alice-Charlie key and Alice can encrypt her message with it
+
+
+if __name__ == '__main__':
+    main()
