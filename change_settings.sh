f947ea335a87d703aa3389cecf199b6ca45fc91d
Peter Shevchenko <petroochcho@gmail.com>
2020-11-06 15:33:53 +0300

    now the script changes correct settings

--- change_settings.sh
+++ change_settings.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#simulaqron set max-qubits 100
-#simulaqron set noisy-qubits on
+
 venv/bin/simulaqron set max-qubits 100
 venv/bin/simulaqron set noisy-qubits off
 venv/bin/simulaqron set t1 1.0
