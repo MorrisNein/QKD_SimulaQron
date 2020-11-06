67f5fb546f728e4f2eff3a9b176f12bdf55b0537
Peter Shevchenko <petroochcho@gmail.com>
2020-11-06 15:38:23 +0300

    folder 'code' renamed to 'simulations'

--- code/3_nodes_XOR_keys/run_nodes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-../../venv/bin/python3 aliceNode.py &
-../../venv/bin/python3 bobNode.py &
-../../venv/bin/python3 charlieNode.py
-
-#python3 aliceNode.py &
-#python3 bobNode.py &
-#python3 charlieNode.py
